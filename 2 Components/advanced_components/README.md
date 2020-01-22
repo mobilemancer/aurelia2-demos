2a7ceb68e990d332ee76dbb61ff16ecf6710c25c
Andreas Wänqvist <andrkarl@gmail.com>
2020-01-22 22:14:22 +0100

    wip: advanced components

---
+++ 2 Components/advanced_components/README.md
@@ -0,0 +1,14 @@
+# advanced_components
+
+This project is bootstrapped by [aurelia/new](https://github.com/aurelia/new).
+
+## Start dev web server
+
+    npm start
+
+## Build the app
+
+Build files to dist folder.
+
+    npm run build
+
---
+++ 2 Components/advanced_components/src/main.ts
@@ -0,0 +1,9 @@
+import Aurelia, { StyleConfiguration } from 'aurelia';
+import { MyApp } from './my-app';
+import * as ComponentRegistry from './components/_components';
+Aurelia
+    .register(StyleConfiguration.cssModulesProcessor(),
+        ComponentRegistry
+    )
+    .app(MyApp)
+    .start();

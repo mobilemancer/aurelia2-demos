317a023aaed7bbf18ed4510a78852b0e3ea41096
Andreas Wänqvist <andrkarl@gmail.com>
2020-01-28 00:12:51 +0100

    wip: routing books

--- 4 Router/basic_setup/src/modules/books/books.ts
+++ 4 Router/basic_setup/src/modules/books/books.ts
@@ -1,3 +1,35 @@
 export class Books {
-
+    public bookList = [
+        {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1965,
+            title: 'Dune',
+        }, {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1969,
+            title: 'Dune Messiah',
+        }, {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1976,
+            title: 'Children of Dune',
+        }, {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1981,
+            title: 'God Emperor of Dune',
+        }, {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1984,
+            title: 'Heretics of Dune',
+        }, {
+            author: 'Frank Herbert',
+            genre: 'Science Fiction',
+            published: 1985,
+            title: 'Chapterhouse: Dune',
+        }
+    ];
 }
--- 4 Router/basic_setup/src/modules/books/details.ts
+++ 4 Router/basic_setup/src/modules/books/details.ts
@@ -1,10 +1,18 @@
 export class Details {
+    public author: string;
+    public published: number;
+    public title: string;
+    public genre: string;
+
     constructor(p) {
-        console.log(p);
     }
 
-    private enter(s) {
-        alert(s);
+    private enter(p) {
+        this.author = p[0];
+        this.genre = p[1];
+        this.published = p[2];
+        this.title = p[3];
+
     }
 
 }

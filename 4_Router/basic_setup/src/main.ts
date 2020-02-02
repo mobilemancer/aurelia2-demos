6e6f3cbea360575542f051c980a885ea24de207c
Andreas Wänqvist <andrkarl@gmail.com>
2020-02-02 11:10:42 +0100

    fix: removed complexity

--- 4_Router/basic_setup/src/main.ts
+++ 4_Router/basic_setup/src/main.ts
@@ -2,14 +2,11 @@ import Aurelia, { RouterConfiguration } from 'aurelia';
 import { MyApp } from './my-app';
 
 import { BookService } from './services/bookService';
-import * as Modules from './modules/_modules';
 
 Aurelia
     .register(
         // need to add the RouterConfig
         RouterConfiguration,
-        // Modules
         BookService
-
     )
     .app(MyApp).start();
